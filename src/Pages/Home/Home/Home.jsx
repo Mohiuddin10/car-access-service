3748b9e58585d94c7ae08d073e80d21058c410d1
Mohiuddin10 <sj.ctg6737@gmail.com>
2024-02-20 23:57:02 +0600

    working on time shedual

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,6 +1,7 @@
 import About from "../About/About";
 import Banner from "../Banner/Banner";
 import Service from "../Service/Service";
+import WorkTime from "../WorkTime/WorkTime";
 
 const Home = () => {
     return (
@@ -8,6 +9,7 @@ const Home = () => {
             <Banner></Banner>
             <About></About>
             <Service></Service>
+            <WorkTime></WorkTime>
         </div>
     );
 };
