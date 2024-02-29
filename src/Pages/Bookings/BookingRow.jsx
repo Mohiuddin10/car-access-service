61ddd18700d2dfd7d12eed49772d5689c34decc3
Mohiuddin10 <sj.ctg6737@gmail.com>
2024-03-01 00:27:02 +0600

    working on confirmed booking

--- src/Pages/Bookings/BookingRow.jsx
+++ src/Pages/Bookings/BookingRow.jsx
@@ -1,15 +1,17 @@
 import { IoIosCloseCircle } from "react-icons/io";
 
-const BookingRow = ({booking}) => {
-    console.log(booking);
-    const {customer:name, serviceName, phone, date, img} = booking;
+const BookingRow = ({booking, handleDelete, handleUpdate}) => {
+    const {customer:name, serviceName, phone, date, img, _id:id, status} = booking;
+
+    
+    
     return (
 
         <tr>
             <th>
                 <label>
                     {/* <input type="checkbox" className="checkbox" /> */}
-                    <button><IoIosCloseCircle className="w-[40px] h-[40px]" /></button>
+                    <button onClick={() => handleDelete(id)}><IoIosCloseCircle className="w-[40px] h-[40px]" /></button>
                 </label>
             </th>
             <td>
@@ -31,7 +33,10 @@ const BookingRow = ({booking}) => {
             </td>
             <td>{date}</td>
             <th>
-                <button className="btn btn-ghost btn-xs bg-[#ff3811] text-white w-[120px] h-[48px]">Pending</button>
+                {
+                    status === "confirm" ? <button className="btn btn-primary">Confirmed</button> :
+                    <button onClick={() => handleUpdate(id)} className="btn btn-ghost btn-xs bg-[#ff3811] text-white w-[120px] h-[48px]">Pending</button>
+                }
             </th>
         </tr>
     );
--- src/Pages/Bookings/Bookings.jsx
+++ src/Pages/Bookings/Bookings.jsx
@@ -4,11 +4,52 @@ import BookingRow from "./BookingRow";
 
 const Bookings = () => {
     const { user } = useContext(AuthContext);
-    console.log(user.email);
     const [bookings, setBookings] = useState([]);
+    const handleDelete = id => {
+        const proceed = confirm("are you sure?");
+        if (proceed) {
+            fetch(`http://localhost:5000/bookings/${id}`, {
+                method: "DELETE"
+            })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if(data.deletedCount > 0){
+                    alert("delete successfull")
+                    const remaining = bookings.filter(booking => booking._id !== id);
+                    setBookings(remaining)
+                }
+            })
+        }
+    }
+
+    const handleUpdate = id => {
+        const proceed = confirm("Are you sure to confirm the service?")
+        if(proceed){
+            fetch(`http://localhost:5000/bookings/${id}`,{
+                method: "PATCH",
+                headers: {
+                    "content-type": "application/json"
+                },
+                body: JSON.stringify({status: "confirm"})
+            })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if(data.modifiedCount > 0) {
+                    // update state 
+                    const remaining = bookings.filter(booking => booking._id !== id);
+                    const updated = bookings.find(booking => booking._id === id);
+                    updated.status = "confirm";
+                    const newBookings = [updated, ...remaining];
+                    setBookings(newBookings);
+                    
+                }
+            })
+        }
+    }
 
     const url = `http://localhost:5000/bookings?email=${user?.email}`;
-    console.log(url);
     useEffect(() => {
         fetch(url)
             .then(res => res.json())
@@ -29,10 +70,10 @@ const Bookings = () => {
                                     <input type="checkbox" className="checkbox" />
                                 </label>
                             </th>
-                            <th>Name</th>
-                            <th>Service Charge</th>
+                            <th className="text-center">Name</th>
+                            <th>Service</th>
                             <th>Date</th>
-                            <th></th>
+                            <th>Status</th>
                         </tr>
                     </thead>
                     <tbody> 
@@ -40,6 +81,8 @@ const Bookings = () => {
                             bookings.map(booking => <BookingRow
                             key={booking._id}
                             booking={booking}
+                            handleDelete={handleDelete}
+                            handleUpdate={handleUpdate}
                             ></BookingRow>)
                         }                  
                     </tbody>
